735eabd3e063f90005787fd67724156bb2841d71
Xuefengguan1 <xuefengguan246@gmail.com>
2024-09-30 20:27:05 -0700

    updated if switch closed, nothing happens, if opened, buzzer ring

--- arduinoSketch/arduinoSketch.ino
+++ arduinoSketch/arduinoSketch.ino
@@ -1,6 +1,20 @@
+int pin = 12;
+int pinBuzzer = 27;
+int switchState = 0;
 void setup(){
-
+  pinMode(pin,INPUT_PULLUP);
+  pinMode(pinBuzzer, OUTPUT);
+  Serial.begin(115200);
 }
 void loop(){
-  
+  switchState = digitalRead(pin);
+
+  if(switchState == LOW){
+    Serial.println("switch is closed");
+    digitalWrite(pinBuzzer,LOW);
+  }else{
+    Serial.println("switch is open");
+    digitalWrite(pinBuzzer,HIGH);
+  }
+  delay(500);
 }
